dc5502df07867c0d7522737d28122f6afc096d29
Precious1324 <aleriwaprecious70@gmail.com>
2024-11-02 01:35:27 +0100

    Addition Update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,12 +8,34 @@ const Footer = () => {
             </div>
             <div className="footer-body">
                 <div className="footer-nav">
+                    <div className="footer-company-nav">
+                        <p className="nav-head">COMPANY</p>
+                        <p className="nav-body">About</p>
+                        <p className="nav-body">Blog</p>
+                        <p className="nav-body">Contact</p>
+                        <p className="nav-body">Jobs</p>
+                    </div>
+                    <div className="footer-company-details">
+                        <p className="nav-head">CONTACT</p>
+                        <p className="nav-body">Phone</p>
+                        <p className="nav-body-2">+234 708 507 3128</p>
+                        <p className="nav-body">Address</p>
+                        <p className="nav-body-2">16, Ogindipe Close, Upston  Close</p>
+                    </div>
 
                 </div>
                 <div className="footer-about">
-
+                    <p className="nav-head">CONSUMER ADVISORY</p>
+                    <p className="about-text">
+                    These statements have not been evaluated by the Food and Drug Administration. This product is not intended to diagnose, treat, cure, or prevent any disease. This product should be used only as directed on the label. All trademarks and copyrights are property of their respective owners and not affiliated with nor do they endorse this product. Results may vary.
+                    </p>
+                    <p className="about-text">By using our website or product, you agree to follow our <span>terms of service.</span></p>
                 </div>
-                <div className="footer-contact"></div>
+                <div className="footer-contact">
+                    <p className="nav-head">GET IN TOUCH</p>
+                    <p className="nav-body">Feel free to get in touch with us vai email</p>
+                    <p className="nav-email">hello@sleepstiq.com</p>               
+                </div>
             </div>
         </div>
     );
